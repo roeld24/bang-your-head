dce1327f7912e89da347fa39b755c0b99009c05e
roeld24 <hroeld@gmail.com>
2025-02-24 09:56:53 +0100

    classe core.Movie

--- src/main/java/ch/supsi/mavendemo/core/core/Movie.java
+++ src/main/java/ch/supsi/mavendemo/core/core/Movie.java
@@ -1,4 +1,37 @@
 package ch.supsi.mavendemo.core.core;
 
 public class Movie {
+    private String title;
+    private String director;
+    private String leadActor;
+    private int year;
+    private int runtime;
+
+    public Movie(String title, String director, String leadActor, int year, int runtime) {
+        this.title = title;
+        this.director = director;
+        this.leadActor = leadActor;
+        this.year = year;
+        this.runtime = runtime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getLeadActor() {
+        return leadActor;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
 }
