4447f3b66c2c53db71d3985d185ee7be2648935f
roeld24 <hroeld@gmail.com>
2025-02-24 09:42:39 +0100

    package invece di moduli

--- app/core/src/main/java/ch/supsi/mavendemo/core/MovieStats.java
+++
@@ -1,6 +0,0 @@
-package ch.supsi.mavendemo.core;
-
-import java.util.*;
-
-public class MovieStats {
-}
--- app/io/src/main/java/ch/supsi/mavendemo/core/Main.java
+++
@@ -1,7 +0,0 @@
-package ch.supsi.mavendemo.core;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
